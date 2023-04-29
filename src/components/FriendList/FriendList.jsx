c21411418a80b78a241a4eeb1fd2bc5a1dcb8db0
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-04-29 23:42:31 +0300

    fix PropTypes

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -21,6 +21,9 @@ FriendsList.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
     })
   ),
 };
